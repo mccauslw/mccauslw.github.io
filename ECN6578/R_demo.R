e84fe6e91e0cc703fa0bae4f5e409061455c257d
William McCausland <mccauslw@mac.com>
2021-01-17 18:55:01 -0500

    Put up Cours 1 slides

---
+++ ECN6578/R_demo.R
@@ -0,0 +1,10 @@
+mu1 = 0.05; sigma1 = 0.1
+mu2 = -0.10; sigma2 = 0.4
+pi = 0.8
+x = seq(-1, 1, by=0.01)
+f1 = dnorm(x, mu1, sigma1)
+f2 = dnorm(x, mu2, sigma2)
+fmelange = pi * f1 + (1-pi) * f2
+plot(x, f1, col='green', 'l')
+lines(x, f2, col='red')
+lines(x, fmelange, col='black')
